d5122dc165be0a343bd051102fe848a2dcebd511
jordanferreiraa <jordanferreira69@gmail.com>
2023-08-10 22:39:00 -0300

    feat: loading message

--- src/form.js
+++ src/form.js
@@ -1,5 +1,6 @@
+import axios from "axios"
 import { startLoading, stopLoading, loadingMessage } from "./loading"
-import { loadVideo } from "./youtube-api"
+import { getVideoId, loadVideo } from "./youtube-api"
 
 const form = document.querySelector('#form')
 
@@ -14,6 +15,9 @@ form.addEventListener('submit', async (e) => {
     const formData = new FormData(form)
     const url = formData.get('url')
     await loadVideo(url)
+
+    loadingMessage('Baixando e convertendo o video')
+    await axios.get('http://localhost:3333/audio?=v' + getVideoId(url))
 
   } catch (error) {
     console.log('[SUBMIT_ERROR] ', error)
