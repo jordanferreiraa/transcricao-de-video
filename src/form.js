657d0d9bdfd7e58ed04c618d08b08b0f5a5c6864
jordanferreiraa <jordanferreira69@gmail.com>
2023-08-11 15:03:00 -0300

    feat: transcribe and render

--- src/form.js
+++ src/form.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { startLoading, stopLoading, loadingMessage } from "./loading"
 import { getVideoId, loadVideo } from "./youtube-api"
+import { transcribeAudio } from "./transcribe"
+import { renderText } from "./render"
 
 const form = document.querySelector('#form')
 
@@ -16,9 +18,11 @@ form.addEventListener('submit', async (e) => {
     const url = formData.get('url')
     await loadVideo(url)
 
-    loadingMessage('Baixando e convertendo o video')
-    await axios.get('http://localhost:3333/audio?v=' + getVideoId(url))
+    // loadingMessage('Baixando e convertendo o video')
+    // await axios.get('http://localhost:3333/audio?v=' + getVideoId(url))
 
+    const data = await transcribeAudio()
+    renderText(data)
   } catch (error) {
     console.log('[SUBMIT_ERROR] ', error)
   } finally {
